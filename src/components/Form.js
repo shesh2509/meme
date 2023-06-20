9df10275e81e7b2cc0f88c5924addb7b7d7d2045
shesh2509 <shesh912002@gmail.com>
2023-06-20 19:03:06 +0530

    firstCommit

---
+++ src/components/Form.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function Form(){
+
+  // const [memeImage, setMemeImage] = React.useState("")
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemes, setAllMemes] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+  }, [])
+
+  function getMemeImage(){
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    const url = allMemes[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
+  }
+
+  function handleChange(event){
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
+  }
+    return (
+      <main>
+        <div className="form">
+            <input 
+                type="text" 
+                className="form--input"
+                placeholder="Top text"    
+                name = "topText"
+                value={meme.topText}
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                className="form--input"
+                placeholder="Bottom text" 
+                name = "bottomText" 
+                value={meme.bottomText}
+                onChange={handleChange}
+            />
+            <button 
+              className="form--button"
+              onClick={getMemeImage}>Get a new meme image 🖼
+              </button>
+        </div>
+        <div className="meme">
+          <img src={meme.randomImage} alt="" className="meme--image"></img>
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+      </main>
+    )
+}
